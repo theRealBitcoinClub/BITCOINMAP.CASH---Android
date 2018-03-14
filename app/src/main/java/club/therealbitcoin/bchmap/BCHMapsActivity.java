3e1eecc1200bb5cffdeca6edaac5e58f9a2ac054
The Real Bitcoin Club <satoshi@therealbitcoin.club>
2018-03-14 22:15:58 +0100

    test actionbar

--- app/src/main/java/club/therealbitcoin/bchmap/BCHMapsActivity.java
+++ app/src/main/java/club/therealbitcoin/bchmap/BCHMapsActivity.java
@@ -10,6 +10,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -47,6 +48,9 @@ public class BCHMapsActivity extends AppCompatActivity implements OnMapReadyCall
 
         Toolbar tb = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(tb);
+        //ActionBar ab = getSupportActionBar();
+        //ab.setDisplayUseLogoEnabled(true);
+        //ab.setDisplayShowTitleEnabled(false);
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
