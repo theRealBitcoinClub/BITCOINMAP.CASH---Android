1dcbdc224e492de2b82b2f8796e087a76d9a297c
Feliz Gubitz <feliz@trbc.io>
2018-03-17 18:11:09 +0100

    added json parse test

--- app/src/test/java/club/therealbitcoin/bchmap/ExampleUnitTest.java
+++ app/src/test/java/club/therealbitcoin/bchmap/ExampleUnitTest.java
@@ -1,5 +1,8 @@
 package club.therealbitcoin.bchmap;
 
+import com.google.android.gms.maps.model.LatLng;
+
+import org.json.JSONObject;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -14,4 +17,14 @@ public class ExampleUnitTest {
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+
+    /*@Test
+    public void testParse() throws Exception {
+        double lat = 42.341234;
+        double lon = 2.235432;
+
+        LatLng bla = WebService.parseLatLng(new JSONObject("{\"lat\":" + lat + ",\"lon\":" + lon +"}"));
+        assertEquals(bla.latitude,lat);
+        assertEquals(bla.latitude,lon);
+    }*/
 }
