52515c06f82ffcf624bf71a19e9cf6d5d73636df
Feliz Gubitz <feliz@trbc.io>
2018-03-21 00:35:45 +0100

    second tab with list

--- app/src/main/java/club/therealbitcoin/bchmap/BCHMapsActivity.java
+++ app/src/main/java/club/therealbitcoin/bchmap/BCHMapsActivity.java
@@ -93,12 +93,6 @@ public class BCHMapsActivity extends AppCompatActivity implements OnMapReadyCall
         setupTabIcons();
     }
 
-    private void setupTabIcons() {
-        tabLayout.getTabAt(0).setIcon(tabIcons[0]);
-        //tabLayout.getTabAt(1).setIcon(tabIcons[1]);
-        //tabLayout.getTabAt(2).setIcon(tabIcons[2]);
-    }
-
     private void initTitleTouchListener() {
         try {
             final int abTitleId = getResources().getIdentifier("action_bar_title", "id", "android");
@@ -119,8 +113,15 @@ public class BCHMapsActivity extends AppCompatActivity implements OnMapReadyCall
 
     private void setupViewPager(ViewPager viewPager, SupportMapFragment mapFragment) {
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(mapFragment, "ONE");
+        adapter.addFragment(mapFragment,null);
+        adapter.addFragment(PopupListFragment.newInstance(),null);
         viewPager.setAdapter(adapter);
+    }
+
+    private void setupTabIcons() {
+        tabLayout.getTabAt(0).setIcon(tabIcons[0]);
+        tabLayout.getTabAt(1).setIcon(tabIcons[1]);
+        //tabLayout.getTabAt(2).setIcon(tabIcons[2]);
     }
 
     private void initMarkersList() {
--- app/src/main/java/club/therealbitcoin/bchmap/Venue.java
+++ app/src/main/java/club/therealbitcoin/bchmap/Venue.java
@@ -1,8 +1,12 @@
 package club.therealbitcoin.bchmap;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import club.therealbitcoin.bchmap.club.therealbitcoin.bchmap.enums.VenueJson;
 import club.therealbitcoin.bchmap.club.therealbitcoin.bchmap.enums.VenueType;
 
-public class Venue {
+public class Venue implements Parcelable{
     String name;
     int iconRes;
     int type;
@@ -19,6 +23,27 @@ public class Venue {
         this.stars = stras;
         this.reviews = rev;
     }
+
+    protected Venue(Parcel in) {
+        name = in.readString();
+        iconRes = in.readInt();
+        type = in.readInt();
+        placesId = in.readString();
+        reviews = in.readInt();
+        stars = in.readDouble();
+    }
+
+    public static final Creator<Venue> CREATOR = new Creator<Venue>() {
+        @Override
+        public Venue createFromParcel(Parcel in) {
+            return new Venue(in);
+        }
+
+        @Override
+        public Venue[] newArray(int size) {
+            return new Venue[size];
+        }
+    };
 
     public int getReviews() {
         return reviews;
@@ -53,5 +78,20 @@ public class Venue {
                 ", type=" + type +
                 ", placesId='" + placesId + '\'' +
                 '}';
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(placesId);
+        dest.writeString(name);
+        dest.writeInt(iconRes);
+        dest.writeInt(type);
+        dest.writeInt(reviews);
+        dest.writeDouble(stars);
     }
 }
