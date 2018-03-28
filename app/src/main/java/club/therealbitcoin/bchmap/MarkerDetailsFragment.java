c5f729ec1a77daaabce88b5fadcd031c1dfcc4e7
Feliz Gubitz <feliz@trbc.io>
2018-03-28 07:24:33 +0200

    reset color of dialog buttons after click

--- app/src/main/java/club/therealbitcoin/bchmap/MarkerDetailsFragment.java
+++ app/src/main/java/club/therealbitcoin/bchmap/MarkerDetailsFragment.java
@@ -2,6 +2,7 @@ package club.therealbitcoin.bchmap;
  
 import android.animation.Animator;
 import android.animation.ValueAnimator;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
@@ -17,6 +18,8 @@ import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.maps.model.LatLng;
 
 import club.therealbitcoin.bchmap.club.therealbitcoin.bchmap.model.VenueType;
 import club.therealbitcoin.bchmap.club.therealbitcoin.bchmap.model.Venue;
@@ -81,7 +84,7 @@ public class MarkerDetailsFragment extends DialogFragment {
 			public void onClick(View v) {
 				Toast.makeText(getContext(), getString(R.string.toast_route_button), Toast.LENGTH_SHORT).show();
 				switchColor(btn_route, true, null);
-				openMapsRoute(venue.placesId);
+				openMapsRoute(venue);
 				resetColorWithDelay(btn_route);
 			}
 
@@ -163,8 +166,8 @@ public class MarkerDetailsFragment extends DialogFragment {
 		new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
             @Override
             public void run() {
-            	btn_route.clearAnimation();
-                //switchColor(btn_route, false, );
+            	//btn_route.clearAnimation();
+                switchColor(btn_route, false, null);
             }
         },300L);
 	}
@@ -194,10 +197,37 @@ public class MarkerDetailsFragment extends DialogFragment {
 	    	animateColorChange(btn, accent, primary, afterAnim);
     }
 
-	private void openMapsRoute(String s) {
+	private void openMapsRoute(Venue v) {
+		/*LatLng coordinates = v.getCoordinates();
+		Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
+			Uri.parse("geo:0,0?q="+ coordinates.latitude+","+coordinates.longitude+" (" + v.name + ")"));
+		startActivity(intent);*/
+
 		Intent i = new Intent(Intent.ACTION_VIEW,
-                Uri.parse(Venue.DIRECTIONS+ s));
+				Uri.parse(Venue.DIRECTIONS+ v.placesId));
 		startActivity(i);
+/*
+		i.setClassName("com.google.android.apps.maps",
+				"com.google.android.maps.MapsActivity");
+		try
+		{
+		}
+		catch(ActivityNotFoundException ex)
+		{
+			try
+			{
+				i = new Intent(Intent.ACTION_VIEW,
+						Uri.parse(Venue.DIRECTIONS+ v.placesId));
+				Log.e(TAG,"NO GOOGLE MAPS ACTIVITY AVAILABLE");
+				startActivity(i);
+			}
+			catch(ActivityNotFoundException innerEx)
+			{
+				Log.e(TAG,"NO MAPS ACTIVITY AVAILABLE");
+				Toast.makeText(getContext(), R.string.toast_install_maps, Toast.LENGTH_LONG).show();
+			}
+		}*/
+
 	}
 
 	private void shareDeepLink() {
